2730b379555ea0c6c5a6f8bb458933a07ce7f713
Sascha Feig <feig@feignet.de>
2025-06-29 09:51:36 +0200

    farbe zurücksetzen wgn. hayabusa

--- myInstall.sh
+++ myInstall.sh
@@ -158,7 +158,7 @@ chmod a+x hayabusa-3.3.0-lin-x64-musl
 ./hayabusa-3.3.0-lin-x64-musl update-rules --quiet
 rm /tmp/hayabusa-json
 cd ..
-reset
+echo -e "\033[0m"
 #
 # https://github.com/tio/tio
 install-apt-package "tio"
